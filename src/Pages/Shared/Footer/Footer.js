a7f2a94661a5914e9a7d9126b71e659658212a96
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-04-18 04:22:24 +0600

    Footer component added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='w-100 text-center container my-auto'>
             <h2>Hello from footer</h2>
         </footer>
     );
